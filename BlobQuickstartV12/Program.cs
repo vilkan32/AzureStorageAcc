cc19d070dbc464fa11533f563c33a327d6dcaa29
velislav1 <velislav1@students.softuni.bg>
2020-08-25 17:51:54 +0300

    second

--- BlobQuickstartV12/Program.cs
+++ BlobQuickstartV12/Program.cs
@@ -1,12 +1,59 @@
-﻿using System;
+﻿using Azure.Storage.Blobs;
+using System;
+using System.IO;
+using System.Threading.Tasks;
 
 namespace BlobQuickstartV12
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Azure Blob storage v12 - .NET quickstart sample\n");
+
+            // Retrieve the connection string for use with the application. The storage
+            // connection string is stored in an environment variable on the machine
+            // running the application called AZURE_STORAGE_CONNECTION_STRING. If the
+            // environment variable is created after the application is launched in a
+            // console or with Visual Studio, the shell or application needs to be closed
+            // and reloaded to take the environment variable into account.
+            string connectionString = Environment.GetEnvironmentVariable("AZURE_STORAGE_CONNECTION_STRING");
+
+            // Create a BlobServiceClient object which will be used to create a container client
+            BlobServiceClient blobServiceClient = new BlobServiceClient(connectionString);
+
+            //Create a unique name for the container
+            string containerName = "quickstartblobs" + Guid.NewGuid().ToString();
+
+
+
+            // Create the container and return a container client object
+            BlobContainerClient containerClient = await blobServiceClient.CreateBlobContainerAsync(containerName);
+
+            // Create a local file in the ./data/ directory for uploading and downloading
+            string localPath = "./data/";
+            string fileName = "quickstart" + Guid.NewGuid().ToString() + ".txt";
+            string localFilePath = Path.Combine(localPath, fileName);
+
+            // Write text to the file
+            await File.WriteAllTextAsync(localFilePath, "Hello, World!");
+
+            // Get a reference to a blob
+            BlobClient blobClient = containerClient.GetBlobClient(fileName);
+
+            Console.WriteLine("Uploading to Blob storage as blob:\n\t {0}\n", blobClient.Uri);
+
+            // Open the file and upload its data
+            using FileStream uploadFileStream = File.OpenRead(localFilePath);
+            await blobClient.UploadAsync(uploadFileStream, true);
+            uploadFileStream.Close();
+
+
+            await blobClient.DownloadAsync();
+
+
         }
+
+
     }
 }
